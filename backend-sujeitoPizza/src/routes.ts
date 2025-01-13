12ed68a048a5513cd767a63e22b6798760742e83
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2025-01-13 10:10:20 -0300

    Detalhes do user

--- backend-sujeitoPizza/src/routes.ts
+++ backend-sujeitoPizza/src/routes.ts
@@ -1,11 +1,16 @@
 import { type Request, type Response, Router } from "express";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 const router = Router();
 
 //-- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/userinfo", isAuthenticated, new DetailUserController().handle);
 
 router.get("/teste", (req: Request, res: Response) => {
 	// throw new Error("Erro ao fazer essa requisição");
